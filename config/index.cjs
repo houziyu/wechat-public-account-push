cae13c3de73585f314584d7028d3c4e5c585fb07
侯梓煜 <469910799@qq.com>
2022-11-17 09:58:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -2,7 +2,7 @@
 
 /**
  * 此项目配置为方便新人使用，已缩减至最简配置。
- * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
+ * 如若想使用更多功能，请查考文档中的 【3. config参数说明】
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU17559TpvZuJPfS9njTS0Jwm6NaOoYdxCCw9ypH',
+      id: 'PDU17583TLlFRD2OJMseQcJVORwhl1HSIpYoj4riL',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -31,12 +31,12 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1997', date: '04-27',
+          type: '*生日', name: ' 小华宝贝', year: '1997', date: '04-27',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
+        //{
+        //  type: '生日', name: '李四', year: '1996', date: '09-31',
+        //},
         {
           type: '节日', name: '相识纪念日', year: '2022', date: '04-08',
         },
@@ -53,7 +53,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '0001',
+  CALLBACK_TEMPLATE_ID: '0002',
 
   CALLBACK_USERS: [
     {
@@ -66,4 +66,3 @@ const USER_CONFIG = {
 }
 
 module.exports = USER_CONFIG
-
